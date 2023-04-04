0facd4ab5f6b87e2248f2e4ae02420e20e574380
mokwongame <ictplusud@gmail.com>
2023-04-04 15:21:46 +0900

    TestHw에 blinkLed() 추가

--- TestHw/TestHw.ino
+++ TestHw/TestHw.ino
@@ -1,3 +1,11 @@
+// nPort: 깜박일 출력 포트; nPeriod: 깜박일 주기(단위: ms)
+void blinkLed(int nPort, int nPeriod) {
+  digitalWrite(nPort, HIGH); // LED on
+  delay(nPeriod/2);
+  digitalWrite(nPort, LOW); // LED off
+  delay(nPeriod/2);
+}
+
 void setup() {  // 한번만 호출
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);  // 13번에 LED 연결; 13번을 출력(OUTPUT)
@@ -5,9 +13,5 @@ void setup() {  // 한번만 호출
 
 void loop() {  // 계속 호출
   // put your main code here, to run repeatedly:
-  // HIGH: 5V, LOW: 0V
-  digitalWrite(13, HIGH); // 13번 포트에 출력을 HIGH
-  delay(1000); // CPU를 1000 msec(밀리초) 동안 지연(delay)
-  digitalWrite(13, LOW); // 13번 포트에 출력을 LOW
-  delay(1000);
+  blinkLed(13, 2000); // 13번 포트를 2000 msec동안 깜박임
 } // 다 끝나면 loop() 함수 처음 부분이 다시 실행

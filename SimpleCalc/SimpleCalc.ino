8ced536671f36a660515a8888f6435f26b00fbf2
mokwongame <ictplusud@gmail.com>
2023-04-11 15:39:36 +0900

    실수와 연산자 입력

--- SimpleCalc/SimpleCalc.ino
+++ SimpleCalc/SimpleCalc.ino
@@ -14,6 +14,22 @@ String input(const String& sTitle) {
   return sInput;
 }
 
+// &: reference(참조) 표시; 값을 가져오지 않고 직접 위치에 접근해서 참조
+// const: 함수 내부에서 입력 변수를 상수(const)로 취급(변경하지 않음)
+double inputDouble(const String& sTitle) {
+  String sX = input(sTitle);
+  double x = sX.toDouble();
+  Serial.println(x);
+  return x;
+}
+
+char inputOp(const String& sTitle) {
+  String sOp = input(sTitle);
+  char op = sOp[0]; // 0번 위치의 문자를 가져옴
+  Serial.println(op);
+  return op;
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(SERIAL_RATE);
@@ -21,7 +37,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  String sX = input("입력 x: ");
-  double x = sX.toDouble();
-  Serial.println(x);
+  double x = inputDouble("입력 x: ");
+  double y = inputDouble("입력 y: ");
+  char op = inputOp("연산자: ");
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "input()으로 double 입력"
+git commit -m "실수와 연산자 입력"
 git push -u origin master

59abab4103dfe24f881e200fe06c42ddaeac244c
mokwongame <ictplusud@gmail.com>
2023-04-18 15:46:18 +0900

    실습 #3(암산 게임) 구현 예시

---
+++ MentslCalc/MentslCalc.ino
@@ -0,0 +1,47 @@
+#define SERIAL_RATE (9600)
+// 연산을 위한 열거형 상수
+enum OpType {
+  OP_ADD,
+  OP_SUB,
+  OP_MUL,
+  OP_DIV,
+  OP_REM
+};
+int nNumMax = 20;  // 암산할 최대 자연수
+int nOpMax = OP_SUB;
+
+String input(const String& sTitle) {
+  Serial.print(sTitle);
+  while (Serial.available() == 0)
+    ;
+  String sInput;
+  while (Serial.available()) {
+    char c = Serial.read();
+    sInput += c;
+    delay(10);  // Serial 속도와 관계
+  }
+  sInput.trim();
+  return sInput;
+}
+
+void printProblem(int a, int b, OpType op) {
+  switch (op) {
+    case OP_ADD: Serial.println(String(a) + " + " + String(b) + " = ?"); break;
+    case OP_SUB: Serial.println(String(a) + " - " + String(b) + " = ?"); break;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(SERIAL_RATE);
+  randomSeed(analogRead(A0));  // 난수 발생 초기화
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int a = random(1, nNumMax + 1);     // 1~nNumMax까지 난수 발생
+  int b = random(1, nNumMax + 1);     // 1~nNumMax까지 난수 발생
+  OpType op = random(0, nOpMax + 1);  // 0~nOpMax까지 난수 발생
+  printProblem(a, b, op);
+  delay(1000);
+}
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "TestRand.ino: 난수 발생 연습"
+git commit -m "실습 #3(암산 게임) 구현 예시"
 git push -u origin master

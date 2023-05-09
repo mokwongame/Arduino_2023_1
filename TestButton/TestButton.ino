eeac79109eb32c57c4e5bcb582e2c440674b7a71
mokwongame <ictplusud@gmail.com>
2023-05-09 16:03:29 +0900

    버튼 상태 확인 함수 추가, LeftRightButtonGame 추가

--- TestButton/TestButton.ino
+++ TestButton/TestButton.ino
@@ -1,6 +1,27 @@
 #define LEFT_BUTTON (2)
 #define RIGHT_BUTTON (3)
 
+enum ButtonType {  // 버튼이 눌러진 상태용 열거형 상수
+  BT_NONE,
+  BT_LEFT,
+  BT_RIGHT,
+  BT_BOTH
+};
+
+ButtonType getButtonInput() {
+  int nInputLeft = digitalRead(LEFT_BUTTON);
+  int nInputRight = digitalRead(RIGHT_BUTTON);
+  ButtonType nResult;
+  if (nInputLeft == LOW) {
+    if (nInputRight == LOW) nResult = BT_BOTH;
+    else nResult = BT_LEFT;
+  } else {
+    if (nInputRight == LOW) nResult = BT_RIGHT;
+    else nResult = BT_NONE;
+  }
+  return nResult;
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -13,5 +34,8 @@ void loop() {
   int nInputLeft = digitalRead(LEFT_BUTTON);
   int nInputRight = digitalRead(RIGHT_BUTTON);
   Serial.println("left = " + String(nInputLeft) + " | right = " + String(nInputRight));
+
+  ButtonType nResult = getButtonInput();
+  Serial.println("button type = " + String(nResult));
   delay(500);
 }
